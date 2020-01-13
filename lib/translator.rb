0a20dc52ac1d952397f34a385f174f0b4a6c1b8f
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-13 19:04:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ emoticons = YAML.load_file(stuff)
 emoticon_hash = Hash.new
 emoticon_hash["get_emoticon"] = Hash.new  
 emoticon_hash["get_meaning"] = Hash.new
-binding.pry
+
 emoticons.each do | english_word, emoticon_set |
 emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
 emoticon_hash["get_meaning"][emoticon_set.last] = english_word
