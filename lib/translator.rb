e3657e7ebdd31d97b711d3488a6782b9a346e635
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-13 19:01:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,9 @@ emoticon_hash["get_meaning"] = Hash.new
 
 emoticons.each do | english_word, emoticon_set |
 emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-
+emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+end
+emoticon_hash
 end
 
 
